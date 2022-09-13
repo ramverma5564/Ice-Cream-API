13aef25669e4f7625bb916f21c536432a6754842
Ram Verma <ramverma5564@gmail.com>
2022-09-13 15:35:19 +0530

    Ice-Cream API Created Successfully.

---
+++ MyApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ArticleViewSet
+
+
+router = DefaultRouter()
+router.register('api',ArticleViewSet)
+
+urlpatterns = [
+
+    path('', include(router.urls))
+]
